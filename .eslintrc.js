212d6f6de96d3cf4f6e4b6f0403780e59d3a0069
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-26 11:29:52 -0400

    styled to page main

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-console': 'off',
+    'react/state-in-constructor': [2, 'never'],
   },
 };
